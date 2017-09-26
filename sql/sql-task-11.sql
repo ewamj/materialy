af7dfeb37c92697a6f2c078d6879ab8a279f8a7b
spajk <github@spajk.com>
2017-09-26 14:13:19 +0200

    zad 11

--- sql/sql-task-11.sql
+++ sql/sql-task-11.sql
@@ -5,3 +5,9 @@
 --   - wyświetl stan magazygnowy dla każdej kategorii - lista w formie
 --     (nazwa kategorii, liczba sztuk)
 
+SELECT p.name, p.price, c.name FROM Products p JOIN Categories c ON p.category_id=c.id;
+SELECT p.name, p.price, c.name FROM Products p, Categories c WHERE p.category_id=c.id;
+
+SELECT p.name, p.price, c.name FROM Products p JOIN Categories c ON p.category_id=c.id WHERE c.name LIKE '%klawiatur%' OR c.name LIKE '%mysz%';
+
+SELECT c.name, sum(p.stock) FROM Products p JOIN Categories c ON p.category_id = c.id GROUP BY c.name;
