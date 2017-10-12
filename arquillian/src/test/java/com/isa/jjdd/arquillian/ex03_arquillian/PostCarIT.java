25ca000cf2c63b32f90ac6809c72c2177f6d0377
Wojciech Makurat <wojtek@infoshareacademy.com>
2017-10-12 14:33:52 +0200

    prosty test arquillian

--- arquillian/src/test/java/com/isa/jjdd/arquillian/ex03_arquillian/PostCarIT.java
+++ arquillian/src/test/java/com/isa/jjdd/arquillian/ex03_arquillian/PostCarIT.java
@@ -2,6 +2,8 @@ package com.isa.jjdd.arquillian.ex03_arquillian;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -16,7 +18,9 @@ public class PostCarIT {
 
     @Deployment
     public static JavaArchive createDeployment() {
-        JavaArchive jar =  null; //ShrinkWrap.create()
+        JavaArchive jar =  ShrinkWrap.create(JavaArchive.class, "it.jar")
+                .addClass(PostCar.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE,"beans.xml");
         return jar;
     }
 
